a1e9dabb6569b78faea91c231d7ee1e536ae16f2
Shiva-76 <shivakumar7ch@gmail.com>
2026-07-26 17:05:24 +0000

    render deployment

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://delivery-backend-api-gfif.onrender.com/predict"
 
 st.set_page_config(page_title="Delivery Estimator", page_icon="🏍️")
 st.title("🏍️ Food Delivery Time Estimator")
